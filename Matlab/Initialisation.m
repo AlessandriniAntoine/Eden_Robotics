8cabaf5ee15b86932dee9bd2d30e5532dad57749
ALESSANDRINI <antoine.alessandrini@inria.fr>
2022-10-09 18:10:12 +0530

    urdf python Matlab

--- Matlab/Initialisation.m
+++ Matlab/Initialisation.m
@@ -1,16 +1,9 @@
-%% Import from Onshape
-% smexportonshape('https://cad.onshape.com/documents/1e4662605519e61dc058d665/w/07df068be4edf05f00108356/e/49578bce04e658622f4f35b3');
-% smimport('AssemblageParfait.xml');
-
-%% Initialisation (A toujours lancer)
-
-% from simulink
-parameters
-load('robot.mat')
-% open_system('Arm.slx')
-% S=sim('Arm.slx')
+%% create robot matrix
+% open_system('robot.slx')
+% S=sim('robot.slx')
 % [robot,importInfo] = importrobot(gcs)
 % robot.DataFormat = 'column';
 
-% from workspace
+%% load robot matrix
+
 % load('/home/alessandrini/Documents/Robot/Matlab/robot')
---
+++ Matlab/previous/parameters.m
@@ -0,0 +1,10 @@
+%Load mechanical parameters
+stl
+
+%Load numerical parameters
+Kp = 0.5;
+Ki = 0.2;
+Kb = 0.8;
+
+sat = 1;
+ki_init = [0 0];
